931af603351cebca12a8b4573999a003ed2848cf
Benjamin Klarer <benjaminklarer@gmail.com>
2022-05-31 20:11:31 -0700

    commit tree f267250ceedad031468dad64335295b92ddf1bff

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(function (tutorial) {
+  return tutorial.split(" ").map(word => (word.charAt(0).toUpperCase() + word.slice(1))) .join(" ")
+})
